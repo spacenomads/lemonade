79e41c38ab2e4daa13943bb69b3898fdf797f300
Carlos Mañas <carlos@spacenomads.com>
2021-10-16 14:34:49 +0200

    Improve post design and fix some graphic details

--- .eleventy.js
+++ .eleventy.js
@@ -12,8 +12,7 @@ module.exports = function (config) {
   config.addPassthroughCopy({'_src/assets/_domain/cname.txt': '/CNAME'});
   config.addPassthroughCopy({'_src/assets/_domain/humans.txt': '/humans.txt'});
   config.addPassthroughCopy({'_src/assets/_icon/favicon.ico': '/favicon.ico'});
-  config.addPassthroughCopy('_src/**/img/*.jpg');
-  config.addPassthroughCopy('_src/**/img/*.png');
+  config.addPassthroughCopy('_src/**/img/*.*');
   config.addPassthroughCopy('_src/assets/fonts');
   config.addPassthroughCopy('_src/assets/css');
   config.addPassthroughCopy('_src/assets/images');
